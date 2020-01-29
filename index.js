57f63ed99a78812ab4148e88575a31ce586528dc
Nicolás Acosta <nadesarrolloweb@gmail.com>
2020-01-29 18:15:14 -0300

    return the visitor ip and country

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const {PORT, NODE_ENV} = process.env;
 
+// responses config
+app.use(bodyParser.json());
+
+// app routes
 app.get('/', (req, res) => {
-  res.send('API Country Data');
+  const {headers} = req;
+  const ip = headers['x-forwarded-for'];
+  const country = headers['cf-ipcountry'];
+
+  if (!ip) {
+    return res.status(400).send();
+  }
+
+  res.send({
+    ip,
+    country
+  });
 });
 
 app.listen(PORT, () => {
