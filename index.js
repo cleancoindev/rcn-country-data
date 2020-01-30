e845e7f966b223685f72ddf0d5ce01c248c4caf0
Nicolas Acosta <nadesarrolloweb@gmail.com>
2020-01-30 12:43:30 -0300

    add cors config

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
-const {PORT, NODE_ENV} = process.env;
+const {PORT, NODE_ENV, CORS_ORIGIN_AVAILABLE} = process.env;
 
 // responses config
 app.use(bodyParser.json());
+app.use(cors({
+  origin: JSON.parse(CORS_ORIGIN_AVAILABLE),
+  optionsSuccessStatus: 200
+}));
 
 // app routes
 app.get('/', (req, res) => {
