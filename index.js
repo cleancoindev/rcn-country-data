5d23d5508f3dbeca2d463367148be18e8d43b780
Nicolás Acosta <nadesarrolloweb@gmail.com>
2020-02-03 14:43:47 -0300

    fix syntax. remove cors from env file

--- index.js
+++ index.js
@@ -7,15 +7,11 @@ const {PORT, NODE_ENV} = process.env;
 
 // responses config
 app.use(bodyParser.json());
-<<<<<<< HEAD
-app.use(cors());
-=======
 app.use(helmet());
 app.use(cors({
-  origin: JSON.parse(CORS_ORIGIN_AVAILABLE),
+  origin: '*',
   optionsSuccessStatus: 200
 }));
->>>>>>> fe1210cfa9f6ef0e6e5b3068e436cf3e5e084b3c
 
 // app routes
 app.get('/', (req, res) => {
